40a6d547f3179cfa31ae892f0efc79eec7cda89a
bigweezyproject <bigweezyproject@gmail.com>
2020-04-20 17:36:05 -0400

    ty/project-setup
    - Installed most of the things
    - Updates

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as classnames from 'classnames'
+
+
+const styles = require('./index.scss')
+
+export const HeaderComponent = () => (
+    <div className={classnames(styles.header)}>header</div>
+)
